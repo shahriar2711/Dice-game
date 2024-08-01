aba24c00ca68f610224f437c0a54659cb119650a
shahriar2711 <atiqshahriar2001@gmail.com>
2024-08-01 22:06:56 +0600

    Create RoleDice.jsx

---
+++ src/components/RoleDice.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const RoleDice = ({currentDice , roleDice}) => {
+
+  return (
+    <DiceContainer>
+      <div className='dice' onClick={roleDice}>
+        <img src={`/images/dice_${currentDice}.png`} alt="" />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  )
+}
+
+export default RoleDice
+
+const DiceContainer = styled.div`
+  margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  .dice{
+    cursor: pointer;
+  }
+
+  p{
+    font-size: 24px;
+    font-weight: 500;
+  }
+`;
